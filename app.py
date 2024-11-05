9c21c8cdd7adfba5b6681d9ad93861af086f37a8
song9fl <songhee.han@fsu.edu>
2024-11-05 12:43:19 -0500

    Update app.py
    final fix

--- app.py
+++ app.py
@@ -95,3 +95,72 @@ if prompt := st.chat_input():
         st.session_state.messages.append({"role": "assistant", "content": output_text})
         st.session_state.citations = response.get("citations", [])
         st.session_state.trace = response.get("trace", {})
+
+# Sidebar for trace information
+trace_types_map = {
+    "Pre-Processing": ["preGuardrailTrace", "preProcessingTrace"],
+    "Orchestration": ["orchestrationTrace"],
+    "Post-Processing": ["postProcessingTrace", "postGuardrailTrace"]
+}
+
+trace_info_types_map = {
+    "preProcessingTrace": ["modelInvocationInput", "modelInvocationOutput"],
+    "orchestrationTrace": ["invocationInput", "modelInvocationInput", "modelInvocationOutput", "observation", "rationale"],
+    "postProcessingTrace": ["modelInvocationInput", "modelInvocationOutput", "observation"]
+}
+
+with st.sidebar:
+    st.title("Trace")
+
+    # Show each trace type in separate sections
+    step_num = 1
+    for trace_type_header in trace_types_map:
+        st.subheader(trace_type_header)
+
+        # Organize traces by step similar to how it is shown in the Bedrock console
+        has_trace = False
+        for trace_type in trace_types_map[trace_type_header]:
+            if trace_type in st.session_state.trace:
+                has_trace = True
+                trace_steps = {}
+
+                for trace in st.session_state.trace[trace_type]:
+                    if trace_type in trace_info_types_map:
+                        trace_info_types = trace_info_types_map[trace_type]
+                        for trace_info_type in trace_info_types:
+                            if trace_info_type in trace:
+                                trace_id = trace[trace_info_type]["traceId"]
+                                if trace_id not in trace_steps:
+                                    trace_steps[trace_id] = [trace]
+                                else:
+                                    trace_steps[trace_id].append(trace)
+                                break
+                    else:
+                        trace_id = trace["traceId"]
+                        trace_steps[trace_id] = [{trace_type: trace}]
+
+                # Show trace steps in JSON similar to the Bedrock console
+                for trace_id in trace_steps.keys():
+                    with st.expander(f"Trace Step {step_num}", expanded=False):
+                        for trace in trace_steps[trace_id]:
+                            trace_str = json.dumps(trace, indent=2)
+                            st.code(trace_str, language="json", line_numbers=trace_str.count("\n"))
+                    step_num += 1
+        if not has_trace:
+            st.text("None")
+
+    # Display citations in the sidebar if available
+    st.subheader("Citations")
+    if st.session_state.citations:
+        citation_num = 1
+        for citation in st.session_state.citations:
+            for retrieved_ref in citation["retrievedReferences"]:
+                with st.expander(f"Citation [{citation_num}]", expanded=False):
+                    citation_str = json.dumps({
+                        "generatedResponsePart": citation["generatedResponsePart"],
+                        "retrievedReference": retrieved_ref
+                    }, indent=2)
+                    st.code(citation_str, language="json", line_numbers=citation_str.count("\n"))
+                citation_num += 1
+    else:
+        st.text("None")
